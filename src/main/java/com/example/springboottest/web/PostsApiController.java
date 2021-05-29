17839257a8969abcce86b8ad7e8ae4826562c041
wintersky-c <wintersky.c@gmail.com>
2021-05-29 16:03:10 +0900

    연습파일 추가

---
+++ src/main/java/com/example/springboottest/web/PostsApiController.java
@@ -0,0 +1,40 @@
+package com.example.springboottest.web;
+
+import com.example.springboottest.service.posts.PostsService;
+import com.example.springboottest.web.dto.PostsResponseDto;
+import com.example.springboottest.web.dto.PostsSaveRequestDto;
+import com.example.springboottest.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.plaf.SpinnerUI;
+import java.nio.file.Path;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+}
